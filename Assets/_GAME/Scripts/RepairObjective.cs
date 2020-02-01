df46c26d481de90993262f548b1655b550c97b1d
Loims <34040415+Loims@users.noreply.github.com>
2020-02-01 17:36:53 -0500

    Add shock objective
    - Add functionality for killing all enemies in radius
    - Add functionality to allow for infinite resource depositing

---
+++ Assets/_GAME/Scripts/RepairObjective.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepairObjective : MonoBehaviour
+{
+    private WaveManager waveManager;
+
+    [SerializeField] private List<GameObject> killableZombies;
+
+    private SphereCollider shockCollider;
+
+    public LayerMask zombieLayer;
+
+    [SerializeField] private float shockRadius;
+    [SerializeField] private int activationCost;
+    [SerializeField] private int depositedMaterialCount;
+
+    private void Awake()
+    {
+        waveManager = GameObject.Find("Singletons").GetComponent<WaveManager>();
+        killableZombies = new List<GameObject>();
+        shockRadius = 5f;
+        activationCost = 1;
+    }
+
+    void Start()
+    {
+        shockCollider = GetComponent<SphereCollider>();
+        shockCollider.radius = shockRadius;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            DepositMaterial();
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, shockRadius);
+    }
+
+    private void DepositMaterial()
+    {
+        depositedMaterialCount++;
+        if(depositedMaterialCount >= activationCost)
+        {
+            depositedMaterialCount = 0;
+            activationCost++;
+            Shock();
+        }
+    }
+
+    private void Shock()
+    {
+        waveManager.ReturnAgentsToPool(killableZombies);
+        killableZombies.Clear();
+        shockRadius += 5f;
+        shockCollider.radius = shockRadius;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Enemy"))
+        {
+            if (!killableZombies.Contains(other.gameObject))
+            {
+                killableZombies.Add(other.gameObject);
+                Debug.Log(other.gameObject);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(killableZombies.Contains(other.gameObject))
+        {
+            killableZombies.Remove(other.gameObject);
+        }
+
+    }
+}
