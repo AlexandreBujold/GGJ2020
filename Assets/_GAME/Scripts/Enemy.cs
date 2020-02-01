d74bdd97c86ad371f91541c948e4ed3ed692a209
Loims <34040415+Loims@users.noreply.github.com>
2020-01-31 21:21:42 -0500

    Add basic managers and AI
    - Add Singletons object with wave manager and AI manager
    - Add test player object to LiamScene
    - Add basic zombie AI

---
+++ Assets/_GAME/Scripts/Enemy.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour, IKillable
+{
+    private bool alive;
+
+    public AIManager aiManager;
+    public GameObject target;
+
+    private void Awake()
+    {
+        aiManager = GameObject.Find("Singletons").GetComponent<AIManager>();
+    }
+
+    private void Start()
+    {
+        aiManager = GameObject.Find("Singletons").GetComponent<AIManager>();
+    }
+
+    private void OnEnable()
+    {
+        alive = true;
+        aiManager.AddToList(this.gameObject);
+    }
+
+    private void OnDisable()
+    {
+        aiManager.RemoveFromList(this.gameObject);
+    }
+
+    public virtual void Kill()
+    {
+        if (aiManager.CheckisInList(this.gameObject))
+        {
+            aiManager.RemoveFromList(this.gameObject);
+        }
+        gameObject.SetActive(false);
+    }
+
+
+}
---
+++ Assets/_GAME/Scripts/WaveManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+
+    public static WaveManager instance;
+
+    public List<GameObject> pooledAgents;
+
+    [SerializeField] private GameObject agentParent; //Object to sort all agents under. Object should exist at Vector3.zero
+
+    [SerializeField] private GameObject zombieBearPrefab;
+
+    public int WaveNumber { get; protected set; } //Current wave
+    public int MaxSpawnedAgents { get; protected set; } //Max enemies on screen at any time
+    public int WaveStrength { get; protected set; } //Max enemies per wave
+
+    public bool waveComplete;
+
+    private void Awake()
+    {
+        /////Singleton Initialization/////
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if(instance != null)
+        {
+            Destroy(gameObject);
+        }
+        /////Singleton Initialization/////
+
+        agentParent = GameObject.Find("PooledAgents");
+        if(agentParent.transform.position != Vector3.zero) //Resets agentParent to Vector3.zero if it has been moved
+        {
+            agentParent.transform.position = Vector3.zero;
+        }
+
+        WaveNumber = 0;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateWaveParameters();
+        Debug.Log(WaveStrength);
+    }
+
+    void Update() //Used to create a number of agents to be pooled. This will be replaced with a spawning algorithm that spawns appropriate AI enemy types
+    {
+        if(pooledAgents.Count <= MaxSpawnedAgents)
+        {
+            int difference = MaxSpawnedAgents - pooledAgents.Count; //Checks the difference between the number of agents in the current wave to the actual number of pooled agents
+
+            for(int i = 0; i < difference + 5; i++) //Adds any missing agents to the pool
+            {
+                GameObject newAgent = Instantiate(zombieBearPrefab, Vector3.zero, Quaternion.identity, agentParent.transform);
+                pooledAgents.Add(newAgent);
+                newAgent.SetActive(false);
+            }
+        }
+
+        ////DEBUG STATEMENT////
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            CreateWaveParameters();
+            
+        }
+        ////DEBUG STATEMENT////
+    }
+
+    void CreateWaveParameters()
+    {
+        WaveNumber++;
+        WaveStrength = 5 + (WaveNumber * 8) + ((int)Mathf.Sqrt(WaveNumber * 8));
+        MaxSpawnedAgents = 10 + (WaveNumber * 5);
+        Debug.Log(MaxSpawnedAgents);
+    }
+}
