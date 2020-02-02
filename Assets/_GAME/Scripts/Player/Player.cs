67b7a0687a875b148bd38fb51680a493cc9e88cd
Alexandre Bujold <c.alexandre.bujold@gmail.com>
2020-02-02 10:50:18 -0500

    Work on Animations for Player
    - Added Player.cs and attached it to Player
    - Added real player model

---
+++ Assets/_GAME/Scripts/Player/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Sirenix.OdinInspector;
+
+public class Player : MonoBehaviour
+{
+
+    [Header("Components")]
+    public PlayerController m_playerController;
+    public Health m_Health;
+    public HandController m_HandController;
+    public CameraController m_cameraController;
+
+    [Header("UI")]
+    public TextMeshProUGUI healthTextValue;
+    public TextMeshProUGUI ammoTextValue;
+    public TextMeshProUGUI waveTextValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
